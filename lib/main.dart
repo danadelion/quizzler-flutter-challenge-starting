5301a8fd14be3f3bf845c3cb2179cd6c473cfd7a
danadelion <dana@linhart.email>
2021-02-21 20:26:30 +0100

    my changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-//TODO: Step 2 - Import the rFlutter_Alert package here.
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'quiz_brain.dart';
+import 'question.dart';
 
 QuizBrain quizBrain = QuizBrain();
 
@@ -30,30 +31,51 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+  List<Question> correctAnswersFinal = [];
+  List<Question> wrongAnswersFinal = [];
 
   void checkAnswer(bool userPickedAnswer) {
     bool correctAnswer = quizBrain.getCorrectAnswer();
 
     setState(() {
-      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
-      //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
-      //HINT! Step 4 Part B is in the quiz_brain.dart
-      //TODO: Step 4 Part C - reset the questionNumber,
-      //TODO: Step 4 Part D - empty out the scoreKeeper.
-
-      //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
       if (userPickedAnswer == correctAnswer) {
         scoreKeeper.add(Icon(
           Icons.check,
           color: Colors.green,
         ));
+        correctAnswersFinal.add(quizBrain.getActualQuestion());
       } else {
         scoreKeeper.add(Icon(
           Icons.close,
           color: Colors.red,
         ));
+        wrongAnswersFinal.add(quizBrain.getActualQuestion());
+      }
+
+      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
+      if (quizBrain.isFinished() == true) {
+        //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
+        int correctAnswerCount = correctAnswersFinal.length;
+        int wrongAnswerCount = wrongAnswersFinal.length;
+        int totalSumOfQuestions = quizBrain.getSumOfQuestions();
+        int percentige =
+            (correctAnswerCount * 100 / totalSumOfQuestions).round();
+        Alert(
+                context: context,
+                title: "Correct Answers/Wrong Answers, percentige",
+                desc: "$correctAnswerCount / $wrongAnswerCount, $percentige %")
+            .show();
+        //HINT! Step 4 Part B is in the quiz_brain.dart
+        //TODO: Step 4 Part C - reset the questionNumber,
+        quizBrain.reset();
+        //TODO: Step 4 Part D - empty out the scoreKeeper.
+        scoreKeeper = [];
+        correctAnswersFinal = [];
+        wrongAnswersFinal = [];
       }
       quizBrain.nextQuestion();
+
+      //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
     });
   }
 
